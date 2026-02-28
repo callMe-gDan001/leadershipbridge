898a13e4ca137357ea96950267b865be0adef0f4
Daniel <dadeniyi689@gmail.com>
2026-03-01 00:30:34 +0100

    Fix: correct typo on line 79 in settings.py

--- LeadershipBridge/settings.py
+++ LeadershipBridge/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(e0y+21%8yim!q03$u9kg)c@l9um@pdxs4+y)-%0l4f^xw%d7j'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -76,7 +76,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-unic
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
